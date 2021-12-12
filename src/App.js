ddd2f8fe6d7adcbc957f69fe81ea05440393d19a
AmanSingh137 <65694984+AmanSingh137@users.noreply.github.com>
2021-12-12 23:13:01 +0530

    cross correlation done and committed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Calculations from './components/Calculations';
 
 function App() {
+  const [val, setVal] = useState("");
+  const [vals, setVals] = useState("");
+  const [face, setFace] = useState(false);
+  const handleChange = (e) => {
+    e.preventDefault();
+    const target = e.target.value;
+    setVal(target);
+  }
+  //let calculation="calc";
+  const changeApp = () => {
+    setFace(true);
+  }
+  const handleChangeagain = (e) => {
+    e.preventDefault();
+    const target = e.target.value;
+    setVals(target);
+  }
+  //let calculation="calc";
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <label>Input Sequence</label>
+        <input type="text" onChange={handleChange} />
+        <br />
+        <label>Impulse Sequence</label>
+        <input type="text" onChange={handleChangeagain} />
+        <br />
+        <button type="button" onClick={changeApp}>Submit</button>
+      </form>
+      {face ? <Calculations values={val} values1={vals} />:null}
     </div>
   );
 }
